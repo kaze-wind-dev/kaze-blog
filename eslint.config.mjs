342410958e6063b5d991bfa515bbb0849462a69f
kaze-wind-dev <kaze.dev.wind@gmail.com>
2025-12-06 13:00:40 +0900

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import globals from "globals";
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2022,
+      },
+    },
+  },
+  {
+    plugins: {
+      "jsx-a11y": jsxA11y,
+    },
+    rules: jsxA11y.configs.recommended.rules,
+  },
+  {
+    ignores: ["dist/**", "node_modules/**", ".astro/**", "public/**"],
+  },
+];
