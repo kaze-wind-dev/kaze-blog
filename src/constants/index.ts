b61452bbc70823a1180381bf18449fe12f005203
kaze-wind-dev <kaze.dev.wind@gmail.com>
2025-12-08 22:07:37 +0900

    feat: Headerコンポーネントの実装
    - Headerコンポーネントの作成・追加
    - Headerコンポーネントのスタイルを追加
    - constants/index.tsの作成
    - styleのファイル分割と整理

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+import type { NavigationItem } from '@/types/navigation';
+
+export const MAIN_NAVIGATION_LIST: NavigationItem[] = [
+  { href: "/notes",name: "Notes"},
+  { href: "/tech",name: "Tech Articles"},
+  { href: "/works",name: "Works"},
+];
+
+export const SUB_NAVIGATION_LIST: NavigationItem[] = [
+  { href: "/contact",name: "Contact"},
+  { href: "/privacy",name: "Privacy Policy"},
+];
+
+export const ALL_NAVIGATION_LIST: NavigationItem[] = [
+    ...MAIN_NAVIGATION_LIST,
+    ...SUB_NAVIGATION_LIST,
+];
