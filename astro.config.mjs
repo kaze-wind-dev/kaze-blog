70b8a36640d7a88c07cd72a3e61ef99c1518da7e
kaze-wind-dev <kaze.dev.wind@gmail.com>
2025-12-17 22:37:41 +0900

    chore: 環境変数を型安全に扱う設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env: {
+    schema: {
+      MICROCMS_API_KEY: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+      MICROCMS_SERVICE_DOMAIN: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+      ZENN_USERNAME: envField.string({
+        context: "client",
+        access: "public",
+        optional: false,
+      }),
+    },
+  },
+});
