b6eee462f7b45c9ec224ab666cc1a773deb42260
kaze-wind-dev <kaze.dev.wind@gmail.com>
2025-12-24 01:01:54 +0900

    feat: WorksSliderコンポーネントの実装
    - WorksSliderの実装
    - WorksSliderのスタイルを追加
    - slider.tsによるスライダー機能の実装

---
+++ src/components/WorksSlider/slider.ts
@@ -0,0 +1,57 @@
+import Splide from "@splidejs/splide";
+import { Intersection } from "@splidejs/splide-extension-intersection";
+
+import "@splidejs/splide/css/core";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const slider = new Splide("#works-slider", {
+    type: "loop",
+    padding: {
+      top: 6,
+      bottom: 6,
+    },
+    perPage: 2,
+    autoplay: "pause",
+    intersection: {
+      inView: {
+        autoplay: true,
+      },
+      outView: {
+        autoplay: false,
+      },
+    },
+    gap: "-12%",
+    interval: 6000,
+    speed: 1000,
+    focus: "center",
+    rewind: true,
+    pauseOnHover: false,
+    pauseOnFocus: true,
+    pauseOnScroll: true,
+    clones: 2,
+    pagination: true,
+    updateOnMove: true,
+    i18n: {
+      prev: "前のスライドに移動する",
+      next: "次のスライドに移動する",
+      first: "最初のスライドに移動する",
+      last: "最後のスライドに移動する",
+      play: "スライドの自動再生",
+      pause: "スライドの自動再生を停止",
+      select: "閲覧したいスライドを選択",
+      slideLabel: "%s / %s",
+    },
+    paginationKeyboard: true,
+    breakpoints: {
+      480: {
+        gap: "-16%",
+        perPage: 1.2,
+      },
+      768: {
+        gap: "-12%",
+        perPage: 1.4,
+      },
+    },
+  });
+  slider.mount({ Intersection });
+});
