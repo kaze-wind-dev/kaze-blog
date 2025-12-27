a6aa430ee32f0c3690ae58a54759e0f8f49130db
kaze-wind-dev <kaze.dev.wind@gmail.com>
2025-12-28 00:21:50 +0900

    chore: 記事の表示数を定数として設定

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,3 +15,10 @@ export const ALL_NAVIGATION_LIST: NavigationItem[] = [
   ...MAIN_NAVIGATION_LIST,
   ...SUB_NAVIGATION_LIST,
 ];
+
+export const TOP_NOTES_LIMIT = 5;
+export const TOP_WORKS_LIMIT = 5;
+export const TOP_TECH_LIMIT = 6;
+export const NOTES_LIMIT = 10;
+export const WORKS_LIMIT = 10;
+export const TECH_LIMIT = 10;
