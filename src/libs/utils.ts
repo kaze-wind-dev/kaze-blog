d24ae0e3709b4008cf6cf37044165e52c385f6fa
kaze-wind-dev <kaze.dev.wind@gmail.com>
2026-01-02 18:36:24 +0900

    feat: highlightCodeBlocks関数の実装

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,7 +1,10 @@
+import * as cheerio from "cheerio";
+import { decode } from "html-entities";
+import { getSingletonHighlighter } from "shiki";
+
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
-
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -17,7 +20,6 @@ export const checkMediaQuery = (cb: (isMobile: boolean) => void): boolean => {
   let isMobile: boolean = false;
   const mediaQuery = window.matchMedia("(min-width: 1024px)");
   mediaQuery.addEventListener("change", (e) => {
-    console.log(e.matches);
     if (e.matches) {
       isMobile = true;
     } else {
@@ -26,4 +28,53 @@ export const checkMediaQuery = (cb: (isMobile: boolean) => void): boolean => {
     cb(isMobile);
   });
   return isMobile;
+};
+
+export const highlightCodeBlocks = async (html: string): Promise<string> => {
+  const $ = cheerio.load(html);
+  const codeBlocks = $("pre code");
+
+  if (codeBlocks.length === 0) return html;
+
+  const highlighter = await getSingletonHighlighter({
+    langs: [
+      "html",
+      "css",
+      "scss",
+      "javascript",
+      "typescript",
+      "php",
+      "astro",
+      "jsx",
+      "tsx",
+      "json",
+      "markdown",
+      "md",
+      "text",
+      "plaintext",
+      "bash",
+      "zsh",
+      "cmd",
+    ],
+    themes: ["github-dark"],
+  });
+  codeBlocks.each((_, elm) => {
+    const $code = $(elm);
+    const rawCode = $code.html() || "";
+    const code = decode(rawCode);
+    const lang = $code.attr("class")?.replace("language-", "") || "plaintext";
+    try {
+      const highlighted = highlighter.codeToHtml(code, {
+        lang: lang,
+        theme: "github-dark",
+      });
+      const pre = $code.parent("pre");
+      pre.replaceWith(highlighted);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        console.warn(`Language not found: ${lang}, error: ${e.message}`);
+      }
+    }
+  });
+  return $.html();
 };
